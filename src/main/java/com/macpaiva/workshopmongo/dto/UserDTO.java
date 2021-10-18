e9e636adebc7cfeac5ceb8b2238158d2e0c12128
MacPaiva <lucasm.paiva@hotmail.com>
2021-10-18 19:55:07 -0300

    Obtendo um usuario por ID

--- src/main/java/com/macpaiva/workshopmongo/dto/UserDTO.java
+++ src/main/java/com/macpaiva/workshopmongo/dto/UserDTO.java
@@ -5,8 +5,8 @@ import java.io.Serializable;
 import com.macpaiva.workshopmongo.domain.User;
 
 public class UserDTO implements Serializable {
-	private static final long  serialVersionId = 1L;
-	
+
+	private static final long serialVersionUID = 1L;
 	
 	private String id;
 	private String name;
